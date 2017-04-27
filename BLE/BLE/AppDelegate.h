277a9eb8f20317f673442238fa24bf64efaf8689
XIA <shaojingtian@163.com>
2017-04-27 09:12:18 +0800

    CoreBluetoothDemo
    Learn how to use bluetooth in iOS

---
+++ BLE/BLE/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BLE
+//
+//  Created by healthplus-mt on 2017/4/26.
+//  Copyright © 2017年 healthplus-mt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BLE/BLE/ViewController.h
@@ -0,0 +1,18 @@
+//
+//  ViewController.h
+//  BLE
+//
+//  Created by healthplus-mt on 2017/4/26.
+//  Copyright © 2017年 healthplus-mt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreBluetooth/CoreBluetooth.h>
+
+@interface ViewController : UIViewController<CBPeripheralDelegate, CBCentralManagerDelegate>
+
+
+
+
+@end
+
